832a1ada46c4f70ee1a5841a57f6101f9c758cad
alexa <48258700+Litteeen@users.noreply.github.com>
2021-01-28 20:20:32 +0100

    update makefiles

--- Tweak/Lisa.h
+++ Tweak/Lisa.h
@@ -1,7 +1,6 @@
-#import <UIKit/UIKit.h>
-#import <Cephei/HBPreferences.h>
 #import <AudioToolbox/AudioServices.h>
 #import <dlfcn.h>
+#import <Cephei/HBPreferences.h>
 
 HBPreferences* preferences;
 UIView* lisaView;
